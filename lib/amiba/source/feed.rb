3c4783bc0bb47a6ac18a42d29f95be29d8486004
Thom May <thom@digital-science.com>
2011-03-10 17:14:57 +0000

    Feeeeeed meeeeeee. Feeed meee NOW!
    (AKA, implement basic builder support for xml feed stuff)

---
+++ lib/amiba/source/feed.rb
@@ -0,0 +1,34 @@
+
+module Amiba
+  module Source
+    class Feed
+      include Amiba::Source
+
+      attr_accessor :type, :name
+      def initialize(fn)
+        self.name, self.type = File.basename(fn, ".builder").split(".")
+      end
+
+      def filename 
+        @filename ||= File.join("feeds", "#{@name}.#{@type}.builder")
+      end
+
+      def content=(c)
+        @content ||= self.new? ? c : File.read(filename)
+      end
+
+      def staged_filename
+        File.join(Amiba::Configuration.staged_dir, filename)
+      end
+
+      def output_filename
+        File.join(Amiba::Configuration.site_dir, "public/#{name}.#{type}")
+      end
+
+      def link
+        URI.escape "/#{name}.#{type}"
+      end
+
+    end
+  end
+end
